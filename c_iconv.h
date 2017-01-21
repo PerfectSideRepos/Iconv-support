167728814506783fa6e89c973dfd1a9752bc19ca
Rockford Wei <rocky@perfect.org>
2017-01-21 05:41:24 -0500

    Adding Module Testing and Mac / Linux Both Supports
    Also improve the module naming to meet PerfectlySoft Quality Standard.

--- c_iconv.h
+++
@@ -1,6 +0,0 @@
-#ifndef _c_iconv_h_
-#define _c_iconv_h_
-
-#include <iconv.h>
-
-#endif
---
+++ ciconv.h
@@ -0,0 +1,8 @@
+#ifndef __C_ICONV__
+#define __C_ICONV__
+#ifdef __APPLE__
+#include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/iconv.h"
+#else
+#include "/usr/include/iconv.h"
+#endif
+#endif
