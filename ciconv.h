254f8886dc5fe694c4a1f60977c059e0f241759a
Rockford Wei <rocky@perfect.org>
2017-01-21 07:05:08 -0500

    specify Mac OS support

--- ciconv.h
+++ ciconv.h
@@ -1,8 +1,4 @@
 #ifndef __C_ICONV__
 #define __C_ICONV__
-#ifdef __APPLE__
 #include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/iconv.h"
-#else
-#include "/usr/include/iconv.h"
-#endif
 #endif
